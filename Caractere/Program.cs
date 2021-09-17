354ee1f489d18bba0bfc0ee1dcc2c719678c709b
Jimmy GOUYAU <gouyau.jimmy@gmail.com>
2021-09-17 15:54:15 +0200

    add all lettre from ASCII

--- Caractere/Program.cs
+++ Caractere/Program.cs
@@ -10,6 +10,34 @@ namespace Caractere
     {
         static void Main(string[] args)
         {
+            char a = (char)97;
+            char b = (char)98;
+            char c = (char)99;
+            char d = (char)100;
+            char e = (char)101;
+            char f = (char)102;
+            char g = (char)103;
+            char h = (char)104;
+            char i = (char)105;
+            char j = (char)106;
+            char k = (char)107;
+            char l = (char)108;
+            char m = (char)109;
+            char n = (char)110;
+            char o = (char)111;
+            char p = (char)112;
+            char q = (char)113;
+            char r = (char)114;
+            char s = (char)115;
+            char t = (char)116;
+            char u = (char)117;
+            char v = (char)118;
+            char w = (char)119;
+            char x = (char)120;
+            char y = (char)121;
+            char z = (char)122;
+           
         }
+        
     }
 }
